1b6fbe01e99f616dd22b68e95a22850b3587429b
Ishan Gupta <ishangupta004@gmail.com>
2019-09-13 19:32:01 +0530

    Add files via upload

---
+++ mac_changer.py
@@ -0,0 +1,22 @@
+import subprocess
+import optparse
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest = "interface", help="interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest = "new_mac", help="new MAC address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] please specify an interface, user --help for more info.")
+    elif not options.new_mac:
+        parser.error("[-] please specify a new mac, user --help for more info.")
+    return options
+
+def change_mac(interface, new_mac):
+    print("Changing MAC address for "+ interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+options = get_arguments()
+change_mac(options.interface, options.new_mac)
